790b50854332732ebc8b76cd9f1ad41e1e68aed7
conyeu <tuanit.nguyenquoc@gmail.com>
2021-08-04 14:57:47 +0700

    0

--- src/js/nodes/FormElement.ts
+++
@@ -1,7 +0,0 @@
-import { Element } from './Element';
-
-export class FormElement extends Element {
-	addElement(node: Element) {
-		throw new Error('Method not implemented.');
-	}
-}
--- src/js/nodes/IObject.ts
+++
@@ -1,3 +0,0 @@
-export interface IObject {
-  equals(object: any): boolean;
-}
